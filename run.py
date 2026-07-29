74426332a8016cce1728ef8f0ad2b9f3a93f2b1e
VRConservation <vance@3point.xyz>
2026-07-29 17:23:12 +0100

    autokill port

--- run.py
+++ run.py
@@ -1,5 +1,21 @@
 """Run the Propongo development server."""
 
+import os
+import subprocess
+import sys
+
+from app.config import Config
+
+PORT = Config.PORT
+
+subprocess.run(
+    ["fuser", "-k", f"{PORT}/tcp"],
+    capture_output=True,
+)
+
+if os.environ.get("DEBUG", "True").lower() in ("true", "1", "yes"):
+    os.environ["FLASK_DEBUG"] = "1"
+
 from app.main import run_server
 
 run_server()
